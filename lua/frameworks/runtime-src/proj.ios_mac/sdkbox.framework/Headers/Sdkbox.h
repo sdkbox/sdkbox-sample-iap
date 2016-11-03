6baf0b057c71d0df73df577190e7e5d089ab0b2a
Nite Luo <darkdukey@gmail.com>
2016-11-03 16:03:41 -0700

    update iap plugin

--- lua/frameworks/runtime-src/proj.ios_mac/sdkbox.framework/Headers/Sdkbox.h
+++ lua/frameworks/runtime-src/proj.ios_mac/sdkbox.framework/Headers/Sdkbox.h
@@ -7,7 +7,7 @@
 #ifndef _CC_SDKBOX_H_
 #define _CC_SDKBOX_H_
 
-#define SDKBOX_VERSION_STR  "sdkbox V2.3.2.0"
+#define SDKBOX_VERSION_STR  "sdkbox V2.3.4.4"
 
 /**
  * Only certain compilers support __attribute__((deprecated)).
